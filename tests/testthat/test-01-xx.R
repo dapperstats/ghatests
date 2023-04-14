910128e533d9403a68562e368d31acef9802aec9
juniperlsimonis <dapperstats@gmail.com>
2023-04-14 11:26:25 -0700

    Update test-01-xx.R

--- tests/testthat/test-01-xx.R
+++ tests/testthat/test-01-xx.R
@@ -6,10 +6,10 @@ test_that("test", {
   
 })
 
-message(runjags::findjags())
-message(runjags::runjags.getOption("jagspath"))
-message(runjags::testjags())
-stop("FUCK")
+warning(runjags::findjags())
+warning(runjags::runjags.getOption("jagspath"))
+
+stop(runjags::testjags())
 
 test_that("test", {
 
